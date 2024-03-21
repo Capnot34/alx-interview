03c69b470625727ac630255785d8a75a986b9028
Capnot34 <jenniferamachree2@gmail.com>
2024-03-21 11:39:12 +0000

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
+"""
+calculates the fewest number of operations needed to result
+in exactly n H characters in the file.
+
+"""
+
 
 def minOperations(n):
-  """
-  This function calculates the minimum number of operations needed to achieve
-  exactly n 'H' characters in a text file.
-
-  Args:
-      n: The target number of 'H' characters.
-
-  Returns:
-      The minimum number of operations required, or 0 if it's impossible.
-  """
-  if n <= 0:
-    return 0
-
-  operations = 0
-  current_count = 1
-
-  while current_count < n:
-    if current_count % 2 == 0:
-      # Double the characters if count is even
-      current_count *= 2
-      operations += 1
-    else:
-      # Paste once for odd count
-      current_count += 1
-      operations += 1
-
-  return operations
+    operation_count = 0
+    number = 2
+    if type(n) is not int or n <= 1:
+        return operation_count
+    while n > 1:
+        if n % number == 0:
+            operation_count += number
+            n /= number
+        else:
+            number += 1
+    return(operation_count)
