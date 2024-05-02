550c32fdd1cd36bc3d29239790934047aed03ad2
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2024-05-02 21:35:42 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
-"""
-Dynamic Coin Change Algorithm
-"""
+""" Making changes """
+
 
 def makeChange(coins, total):
-    """
-    Calculates the fewest number of coins needed to reach the total amount.
+    """ Generate changes needed to reach total
 
     Args:
-        coins (list): List of available coin denominations.
-        total (int): Total amount needed.
-
-    Returns:
-        int: The fewest number of coins needed to reach the total, or -1 if it's not possible.
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
     """
-    if total < 1:
+    if total <= 0:
         return 0
-    
+    check = 0
+    temp = 0
     coins.sort(reverse=True)
-    coins_used = 0
-    amount_checked = 0
-    
-    for coin in coins:
-        while amount_checked < total:
-            amount_checked += coin
-            coins_used += 1
-        
-        if amount_checked == total:
-            return coins_used
-        
-        amount_checked -= coin
-        coins_used -= 1
-    
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
     return -1
